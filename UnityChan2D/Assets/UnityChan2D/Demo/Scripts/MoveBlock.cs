7382601944ee091e0d53f96947d8ec94550d2dad
akihasakura <akihasakurasakuya@gmail.com>
2014-11-17 18:50:25 +0900

    オブジェクトファイルです

---
+++ UnityChan2D/Assets/UnityChan2D/Demo/Scripts/MoveBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock : MonoBehaviour {
+
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move (transform.right * -1);
+	}
+
+	public void Move (Vector2 direction)
+	{
+		rigidbody2D.velocity = direction * speed;
+	}
+}

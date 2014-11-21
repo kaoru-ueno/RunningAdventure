76a4a752a46667c73dc32ffe4fa23f88282fc8a4
akihasakura <akihasakurasakuya@gmail.com>
2014-11-21 17:16:31 +0900

    アプデ

--- RunningAdventure/Assets/FloorControl.cs
+++ RunningAdventure/Assets/FloorControl.cs
@@ -10,10 +10,10 @@ public class FloorControl : MonoBehaviour {
 	//private Vector3	initial_position;
 	
 	// 床の幅（X方向）.
-	public	static float	WIDTH = 4.0f;
+	public	static float	WIDTH = 5.0f;
 	
 	// 床モデルの数.
-	public static int		MODEL_NUM = 7;
+	public static int		MODEL_NUM = 2;
 	
 	void	Start() 
 	{

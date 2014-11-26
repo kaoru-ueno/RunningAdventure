4f8b5f5cefb99e153b62a207e0a95b51ef5b4fd4
kaoru-ueno <YZK@uenokaoru-no-MacBook-Pro.local>
2014-11-27 02:52:22 +0900

    スタートシーン制作中

--- RunningAdventure/Assets/StartScene.cs
+++ RunningAdventure/Assets/StartScene.cs
@@ -5,7 +5,12 @@ public class StartScene : MonoBehaviour {
 
 	private int ButtonWidth = 300;
 	private int ButtonHeight = 70;
+	private int BgPanelWidth = 552;
+	private int BgPanelHeight = 316;
+	private int TextFieldWidth = 320;
+	private int TextFieldHeight = 50;
 
+	private string UserName = "aaa";
 	private int maxLength = 20;
 		
 	public GUIStyle startButtonStyle;
@@ -13,14 +18,14 @@ public class StartScene : MonoBehaviour {
 	public GUIStyle rankingButtonStyle;
 	public GUIStyle kanryoButtonStyle;
 
-	public Texture2D UserName;
+	public Texture2D bgPanel;
 
 	private bool is_firstPlay = true;
 
 	// Use this for initialization
 	void Start () {
 		gameObject.renderer.enabled = false;
-		GameObject.Find("UserName").renderer.enabled = false;
+		//GameObject.Find("UserName").renderer.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -32,27 +37,31 @@ public class StartScene : MonoBehaviour {
 		if (is_firstPlay) {
 			// グループの背景にTexture2Dのテクスチャを指定.
 			GUIStyle style = new GUIStyle();
-			style.normal.background = UserName;
-			GUI.BeginGroup(new Rect(Screen.width / 2 - 552 / 2, Screen.height / 2 - 316 / 2, 552, 316), style);
+			style.normal.background = bgPanel;
+			GUI.BeginGroup(new Rect(Screen.width / 2 - BgPanelWidth / 2, Screen.height / 2 - BgPanelHeight / 2, BgPanelWidth, BgPanelHeight), style);
 
-			// 
-			//GameObject.Find("UserName").renderer.enabled = true;
+			// 入力フィールドを作る
+			Rect rect1 = new Rect(BgPanelWidth / 2 - TextFieldWidth / 2, (BgPanelHeight / 2 - TextFieldHeight / 2)-30, TextFieldWidth, TextFieldHeight);
+			UserName = GUI.TextField(rect1, UserName, maxLength, inputStyle);
+			//フォーカスの当て方が分からない！あとフォーカスカーソルの色の変え方も。
+			//GUI.SetNextControlName("NameField");
+			//GUI.FocusControl("NameField");
+			//GUI.FocusWindow(NameField);
 
-			Rect rect1 = new Rect(10, 10, 300, 30);
-			GUI.TextField(rect1, "Stand by Ready!!", maxLength, inputStyle);
-
-			if (GUI.Button(new Rect(552 / 2 - ButtonWidth / 2, (316 / 2 - ButtonHeight / 2)+90, ButtonWidth, ButtonHeight),"", kanryoButtonStyle)){
-				print ("入力完了！をクリックしました");
-				//Application.LoadLevel ("Test");
+			if (GUI.Button(new Rect(BgPanelWidth / 2 - ButtonWidth / 2, (BgPanelHeight / 2 - ButtonHeight / 2)+90, ButtonWidth, ButtonHeight),"", kanryoButtonStyle)){
+				if(UserName != ""){
+					print ("名前は " + UserName + "です。");
+					is_firstPlay = false;
+				}else{
+					print ("名前を入力してください。");
+				}
 			}
-
+			GUI.EndGroup ();
 		}else{
 			// タイトルを表示する
 			gameObject.renderer.enabled = true;
 
 			// ボタンを表示する
-			//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート");
-			//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング");
 			if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight),"", startButtonStyle)){
 				print ("スタートをクリックしました");
 				Application.LoadLevel ("Test");
---
+++ RunningAdventure/Assets/UseNameInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseNameInput : MonoBehaviour {
+	private int ButtonWidth = 300;
+	private int ButtonHeight = 70;
+	private int maxLength = 20;
+	public GUIStyle inputStyle;
+	public Texture2D UserName;
+	public GUIStyle kanryoButtonStyle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		// グループの背景にTexture2Dのテクスチャを指定.
+		GUIStyle style = new GUIStyle();
+		style.normal.background = UserName;
+		GUI.BeginGroup(new Rect(Screen.width / 2 - 552 / 2, Screen.height / 2 - 316 / 2, 552, 316), style);
+			
+		// 
+			//GameObject.Find("UserName").renderer.enabled = true;
+
+		Rect rect1 = new Rect(10, 10, 300, 30);
+		GUI.TextField(rect1, "Stand by Ready!!", maxLength, inputStyle);
+			
+		if (GUI.Button(new Rect(552 / 2 - ButtonWidth / 2, (316 / 2 - ButtonHeight / 2)+90, ButtonWidth, ButtonHeight),"", kanryoButtonStyle)){
+			print ("入力完了！をクリックしました");
+			//Application.LoadLevel ("Test");
+		}
+	}
+}

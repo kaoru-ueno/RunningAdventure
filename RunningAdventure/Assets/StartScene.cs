0073a3a1ef3ca9b4930a2f3c3d540c896e6646ad
kaoru-ueno <kaoruueno.1013@gmail.com>
2014-11-26 21:05:33 +0900

    スタートシーン作成中

--- RunningAdventure/Assets/StartScene.cs
+++ RunningAdventure/Assets/StartScene.cs
@@ -6,9 +6,14 @@ public class StartScene : MonoBehaviour {
 	private int ButtonWidth = 300;
 	private int ButtonHeight = 70;
 
+	public GUIStyle startButtonStyle;
+	public GUIStyle rankingButtonStyle;
+
+	private bool is_firstPlay = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		gameObject.renderer.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -17,16 +22,26 @@ public class StartScene : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		// ボタンを表示する
-		//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート");
-		//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング");
-		if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート")){
-			print ("スタートをクリックしました");
-			Application.LoadLevel ("Test");
-		}
-		if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング")){
-			print ("ランキングをクリックしました");
-			Application.LoadLevel ("Ranking");
+		if (is_firstPlay) {
+			Rect rect1 = new Rect(10, 10, 400, 30);
+			GUI.TextField(rect1, "Stand by Ready!!");
+
+
+		}else{
+			// タイトルを表示する
+			gameObject.renderer.enabled = true;
+
+			// ボタンを表示する
+			//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート");
+			//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング");
+			if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight),"", startButtonStyle)){
+				print ("スタートをクリックしました");
+				Application.LoadLevel ("Test");
+			}
+			if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+120, ButtonWidth, ButtonHeight),"", rankingButtonStyle)){
+				print ("ランキングをクリックしました");
+				Application.LoadLevel ("Ranking");
+			}
 		}
 	}
 }

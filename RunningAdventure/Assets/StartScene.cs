314e7a3390b06099c31592727fd9e6f6438e8a9f
kaoru-ueno <kaoruueno.1013@gmail.com>
2014-11-26 18:58:15 +0900

    スタートシーン追加

---
+++ RunningAdventure/Assets/StartScene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScene : MonoBehaviour {
+
+	private int ButtonWidth = 300;
+	private int ButtonHeight = 70;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		// ボタンを表示する
+		//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート");
+		//GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング");
+		if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+20, ButtonWidth, ButtonHeight), "ゲームスタート")){
+			print ("スタートをクリックしました");
+			Application.LoadLevel ("Test");
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 - ButtonWidth / 2, (Screen.height / 2 - ButtonHeight / 2)+100, ButtonWidth, ButtonHeight), "ランキング")){
+			print ("ランキングをクリックしました");
+			Application.LoadLevel ("Ranking");
+		}
+	}
+}

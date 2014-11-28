1fa9d9a5d6b5abf27612dea5b591d125bee0543c
inoinouemasato <idogguest@gmail.com>
2014-11-28 21:01:01 +0900

    エンドスコア更新

--- RunningAdventure/Assets/UnityChan2D/Demo/Scripts/EndScore.cs
+++ RunningAdventure/Assets/UnityChan2D/Demo/Scripts/EndScore.cs
@@ -5,16 +5,22 @@ public class EndScore : MonoBehaviour
 	// スコアを表示するGUIText
 	public GUIText endHighScoreGUIText;
 	public GUIText endScoreGUIText;
+	public GUIText kmGUIText;
+	public GUIText clistalScoreGUIText;
+	private GameObject main_camera = null;
 
 	void Start ()
 	{
-
+		this.main_camera = GameObject.FindGameObjectWithTag("MainCamera");
+		GameObject.Find("Km").guiText.text = "";
 	}
 	
 	void Update ()
 	{
-
 		endHighScoreGUIText.text = Score.highScore.ToString ();
-		endScoreGUIText.text = Score.score.ToString ();
+//		endScoreGUIText.text = Score.score.ToString ((””));
+		clistalScoreGUIText.text = Score.score.ToString ("獲得したクリスタルは" + ("0") + "点です");
+		Vector3	camera_position = this.main_camera.transform.position;
+		kmGUIText.text = camera_position.x.ToString ("あなたの走行距離は" + ("0") + "mです");
 	}
 }

55be1e3b70be74e8d3727bfe4084a4921b01cd88
kaoru-ueno <kaoruueno.1013@gmail.com>
2014-12-01 18:25:29 +0900

    ゲージのところ

--- RunningAdventure/Assets/FeverGaugeControl.cs
+++ RunningAdventure/Assets/FeverGaugeControl.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class FeverGaugeControl : MonoBehaviour {
 	// ゲージ用
 	private GameObject gaugeObject;
+	private GameObject feverGaugeObject;
 	private float gaugeScale = 1;
 	private int playerScore;
 	private int localScore;
@@ -12,22 +13,42 @@ public class FeverGaugeControl : MonoBehaviour {
 	void Start () {
 		// ゲージの取得
 		gaugeObject = transform.FindChild("Bar").gameObject;
+		feverGaugeObject = transform.FindChild("Fever").gameObject;
+
+		// 初期化
+		init ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		localScore = Score.score;
-		ChangeGauge ();
+
+		localScore = Score.score - playerScore;
+		if(localScore < FeverScore){
+			ChangeGauge ();
+		}else{
+			// FeverTime!
+			// フィーバーゲージ表示
+			gaugeObject.renderer.enabled = false;
+			feverGaugeObject.renderer.enabled = true;
+		}
 	}
 	
 	// ゲージ描画用(ダメージからゲージのスケールと位置を求め変更)
 	void ChangeGauge(){
 		gaugeScale = Mathf.Round(10.0F* localScore / FeverScore) / 10;
-		//Vector3 scale = gaugeObject.transform.localScale;
-		//scale.x = gaugeScale;
-		//gaugeObject.transform.localScale = scale;
 		gaugeObject.transform.localScale = new Vector3(gaugeScale, 1,1);
-		float gaugePosition = (gaugeScale - 1) / 2;
-		gaugeObject.transform.localPosition = new Vector3(gaugePosition, 0, 0);;
+		float gaugePosition = 5f *(gaugeScale - 1) / 2;
+		gaugeObject.transform.localPosition = new Vector3(gaugePosition, 0, 0);
+	}
+
+	// 初期化
+	void init(){
+		// スコアを0にする
+		localScore = 0;
+		// スコアを取得
+		playerScore = Score.score;
+		// フィーバーゲージ非表示
+		gaugeObject.renderer.enabled = true;
+		feverGaugeObject.renderer.enabled = false;
 	}
 }

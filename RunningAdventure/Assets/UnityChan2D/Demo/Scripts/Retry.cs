43e2da648aeb76d24e0840333e9134da01166fce
inoinouemasato <idogguest@gmail.com>
2014-12-02 16:43:57 +0900

    エンドスコア　追加

---
+++ RunningAdventure/Assets/UnityChan2D/Demo/Scripts/Retry.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Retry : MonoBehaviour {
+
+//	public Texture2D star;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+/*	void Update () {
+		if(Input.GetMouseButton(0)){
+			Application.LoadLevel("Start");
+	}
+	}*/
